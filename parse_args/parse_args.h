70d7f834f7bfcae49fd92f7d1f6c2158b10d7340
graigs-list <kovacocyj27@bolles.org>
2023-12-21 12:23:05 -0500

    forgot a free() call and added it

--- parse_args/parse_args.h
+++ parse_args/parse_args.h
@@ -2,6 +2,7 @@
 #define PARSE_ARGS_H
 
 #include <string.h>
+#include <stdlib.h>
 #include "../file_mode/file_mode.h"
 
 void parse_args(int argc, char* argv[], file_mode* mode, reliability* reliability, char* name[]) {
@@ -10,6 +11,8 @@ void parse_args(int argc, char* argv[], file_mode* mode, reliability* reliabilit
     if (file_name) {
         (*name) = file_name;
     }
+
+    free(file_name);
 }
 
 #endif
