74a8cd2a13ae436dadc55461b9c30c0e676e0a99
graigs-list <kovacocyj27@bolles.org>
2023-12-21 12:20:47 -0500

    updated production exe, now parses given file name

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
 #include "./file_mode/file_mode.h"
+#include "./parse_args/parse_args.h"
 
 #define EXIT_SUCCESS 0
 #define EXIT_ERROR 1
 
 file_mode current_mode;
 reliability current_reliability;
+char* current_name;
 
 int main(int argc, char* argv[]) {
-    /*if (argc >= 0) {
+    if (argc <= 0) {
         printf("No file supplied");
         return EXIT_ERROR;
-    }*/
+    }
 
-    current_reliability = UNRELIABLE;
+    parse_args(argc, argv, &current_mode, &current_reliability, &current_name);
 
-    printf("%i", current_reliability);
+    printf(current_name);
 
     return EXIT_SUCCESS;
 }
--- parse_args/parse_args.h
+++ parse_args/parse_args.h
@@ -1,9 +1,15 @@
 #ifndef PARSE_ARGS_H
 #define PARSE_ARGS_H
 
+#include <string.h>
 #include "../file_mode/file_mode.h"
 
-void parse_args(int argc, char* argv[], file_mode mode, file_reliability reliability) {
+void parse_args(int argc, char* argv[], file_mode* mode, reliability* reliability, char* name[]) {
+    char* file_name;
+    strcpy(file_name, argv[1]);
+    if (file_name) {
+        (*name) = file_name;
+    }
 }
 
 #endif
