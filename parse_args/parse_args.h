f06cf26e8477866acf75221b2819fffb1f7eb1b0
graigs-list <kovacocyj27@bolles.org>
2023-12-21 12:10:39 -0500

    more includes

--- parse_args/parse_args.h
+++ parse_args/parse_args.h
@@ -1,8 +1,9 @@
 #ifndef PARSE_ARGS_H
 #define PARSE_ARGS_H
 
-void parse_args(int argc, char* argv[], ) {
+#include "../file_mode/file_mode.h"
 
+void parse_args(int argc, char* argv[], file_mode mode, file_reliability reliability) {
 }
 
 #endif
