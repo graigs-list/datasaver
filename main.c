d5702d61725b0be0315a3fb5f93b612fb2dcc755
graigs-list <kovacocyj27@bolles.org>
2023-12-21 12:50:36 -0500

    fixed return codes, and added support for parsing file mode and file reliability

--- main.c
+++ main.c
@@ -4,21 +4,14 @@
 #include "./parse_args/parse_args.h"
 
 #define EXIT_SUCCESS 0
-#define EXIT_ERROR 1
+#define EXIT_ERROR -1
 
 file_mode current_mode;
 reliability current_reliability;
 char* current_name;
 
 int main(int argc, char* argv[]) {
-    if (argc <= 0) {
-        printf("No file supplied");
-        return EXIT_ERROR;
-    }
-
-    parse_args(argc, argv, &current_mode, &current_reliability, &current_name);
-
-    printf(current_name);
+    if (parse_args(argc, argv, &current_mode, &current_reliability, &current_name)) {return EXIT_ERROR;}
 
     return EXIT_SUCCESS;
 }
--- parse_args/parse_args.h
+++ parse_args/parse_args.h
@@ -5,14 +5,41 @@
 #include <stdlib.h>
 #include "../file_mode/file_mode.h"
 
-void parse_args(int argc, char* argv[], file_mode* mode, reliability* reliability, char* name[]) {
+int parse_args(int argc, char* argv[], file_mode* mode, reliability* reliability, char* name[]) {
+    if (argc <= 0) {
+        printf("No file supplied");
+        return -1;
+    }
+
+    if (argc <= 1) {
+        printf("No file mode supplied");
+        return -1;
+    }
+
+    if (argc <= 2) {
+        printf("No file reliability supplied");
+        return -1;
+    }
+
     char* file_name;
-    memcpy (file_name, argv[1], strlen(argv[1]) + 1);
+    memcpy(file_name, argv[1], strlen(argv[1]) + 1);
     if (file_name) {
         (*name) = file_name;
     }
 
+    int _file_mode = argv[2][0] - '0';
+    if (_file_mode) {
+        (*mode) = (int)_file_mode;
+    }
+
+    int _file_reliability = argv[3][0] - '0';
+    if (_file_reliability) {
+        (*reliability) = (int)_file_reliability;
+    }
+
     free(file_name);
+
+    return 0;
 }
 
 #endif
